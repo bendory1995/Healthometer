a2d6937a044ebfb279599e40da39dbf726dced91
Alex Oh <oalex@knights.ucf.edu>
2021-08-20 12:19:39 +0900

    initialized firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAMsDIqRW-X_LZjABoPH88vb1tpw0nbXZQ",
+    authDomain: "healthometer-3c98b.firebaseapp.com",
+    projectId: "healthometer-3c98b",
+    storageBucket: "healthometer-3c98b.appspot.com",
+    messagingSenderId: "394094378509",
+    appId: "1:394094378509:web:011722f680c32ef8a29f44",
+    measurementId: "G-H3DVHYMKCY"
+  };
+  
+firebase.initializeApp(firebaseConfig);
